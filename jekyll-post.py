7b870b66ca512d306e23f8a013c2d59d2416d071
Sibin Arsenijevic <sarsenijevic@gogoair.com>
2016-08-09 02:49:12 +0200

    fixed template indentation

--- jekyll-post.py
+++ jekyll-post.py
@@ -11,6 +11,15 @@ parser.add_argument("-t", "--tags", help="Comma separated list of tags. Defaults
 parser.add_argument("-c", "--categories", help="Comma separated list of categories. Defaults to 'blog'.")
 args = parser.parse_args()
 
+post_template = """---
+layout: post
+title:  {title}
+date:   {post_date}
+categories: {post_categories}
+tags: [{post_tags}]
+---
+"""
+
 
 def sanitize_post_title(title):
     """
@@ -46,17 +55,10 @@ def main():
     filename = "_{}-{}.md".format(date_today, sanitize_post_title(post_title))
     file_path = os.path.join(os.getcwd(), "_posts", filename)
 
-    post_header = """---
-    layout: post
-    title:  {title}
-    date:   {post_date}
-    categories: {post_categories}
-    tags: [{post_tags}]
-    ---
-    """.format(title=post_title,
-               post_date=datetime_post,
-               post_categories=post_categories,
-               post_tags=post_tags)
+    post_header = post_template.format(title=post_title,
+                                       post_date=datetime_post,
+                                       post_categories=post_categories,
+                                       post_tags=post_tags)
 
     with open(file_path, 'w') as post_file:
         post_file.write(post_header)
