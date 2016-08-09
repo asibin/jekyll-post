b846d74d4afd53d637605c37819bab0ab316a701
Sibin Arsenijevic <sarsenijevic@gogoair.com>
2016-08-09 02:30:20 +0200

    Initial commit, basic functionality

---
+++ README.md
@@ -0,0 +1,19 @@
+# Jekyll post template
+
+Quick and ~~dirty~~ simple script to create Jekyll post template.
+
+Not too much error handling :)
+
+## Usage
+
+Run from root of website dir. 
+
+File will be created in _posts.
+
+File is created as unpublished post. 
+
+Remove `_` after you are done editing.
+
+## Arguments
+
+Run script with `-h` to see all the parameters.
---
+++ jekyll-post.py
@@ -0,0 +1,68 @@
+import argparse
+import datetime
+import sys
+import re
+import os
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("title", help="Post title. Only alfanumerics and dashes are allowed. Required.")
+parser.add_argument("-t", "--tags", help="Comma separated list of tags. Defaults to 'blog'.")
+parser.add_argument("-c", "--categories", help="Comma separated list of categories. Defaults to 'blog'.")
+args = parser.parse_args()
+
+
+def sanitize_post_title(title):
+    """
+    Sanitizes post title for filename
+    :param title: str: Title of blog post
+    :return: str: Sanitized title containing only alafanumerics and dashes
+    """
+    title = title.lower().replace(' ', '-')
+    if re.match('^[\w-]+$', title) is not None:
+        return title
+    else:
+        print "Only alfanumerics and dashes are allowed"
+        sys.exit(1)
+
+
+def main():
+    post_title = args.title
+
+    if not args.categories:
+        post_categories = "blog"
+    else:
+        post_categories = " ".join(args.categories.split(','))
+
+    if not args.tags:
+        post_tags = "blog"
+    else:
+        post_tags = ", ".join(args.tags.split(','))
+
+    datetime_today = datetime.datetime.today()
+    date_today = datetime_today.date()
+    datetime_post = datetime_today.strftime("%Y-%m-%d %H:%M:%S")
+
+    filename = "_{}-{}.md".format(date_today, sanitize_post_title(post_title))
+    file_path = os.path.join(os.getcwd(), "_posts", filename)
+
+    post_header = """---
+    layout: post
+    title:  {title}
+    date:   {post_date}
+    categories: {post_categories}
+    tags: [{post_tags}]
+    ---
+    """.format(title=post_title,
+               post_date=datetime_post,
+               post_categories=post_categories,
+               post_tags=post_tags)
+
+    with open(file_path, 'w') as post_file:
+        post_file.write(post_header)
+
+    print "Post: {} created!".format(filename)
+
+
+if __name__ == '__main__':
+    main()
